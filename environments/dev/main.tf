b9d66d44880e501988fbd52cec73dab2a9f89de9
GokulakannanRN <Gokulakannan1306@gmail.com>
2026-08-30 23:39:46 +0530

    added terrafile and jenkins file

---
+++ environments/dev/main.tf
@@ -0,0 +1,15 @@
+module "vpc" {
+  source               = "../../modules/vpc"
+  vpc_cidr             = "10.0.0.0/16"
+  subnet_cidr          = "10.0.1.0/24"
+  subnet_az            = "${var.aws_region}a"
+  env                  = "dev"
+}
+module "ec2" {
+  source        = "../../modules/ec2"
+  ami_id        = "ami-0332d564d76dbd8d6"
+  instance_type = "t3.micro"
+  subnet_id     =  module.vpc.subnet_id
+  ec2_count     = 1
+  env           = "dev"
+}
