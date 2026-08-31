c0b390cbeaea73091a8d90898454e2000e05fb16
GokulakannanRN <Gokulakannan1306@gmail.com>
2026-08-31 22:39:17 +0530

    destroy all

--- environments/production/main.tf
+++ environments/production/main.tf
@@ -10,12 +10,12 @@ module "ec2" {
   ami_id        = "ami-0332d564d76dbd8d6"
   instance_type = "t3.micro"
   subnet_id     =  module.vpc.subnet_id
-  ec2_count     = 2
+  ec2_count     = 0
   env           = "production"
 }
 module "s3" {
   source        = "../../modules/s3"
   bucket        = "kannan-project"
   env           = "production"
-  s3_count      = 2
+  s3_count      = 0
 }*/
