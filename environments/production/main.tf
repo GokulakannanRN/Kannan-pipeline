63f334f009f842f5b421c2147aa86ccc3aeeb54a
GokulakannanRN <Gokulakannan1306@gmail.com>
2026-08-31 22:39:48 +0530

    destroy all

--- environments/production/main.tf
+++ environments/production/main.tf
@@ -1,4 +1,4 @@
-/*module "vpc" {
+module "vpc" {
   source               = "../../modules/vpc"
   vpc_cidr             = "10.0.0.0/16"
   subnet_cidr          = "10.0.1.0/24"
@@ -18,4 +18,4 @@ module "s3" {
   bucket        = "kannan-project"
   env           = "production"
   s3_count      = 0
-}*/
+}
