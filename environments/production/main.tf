17bc57e07e199d4a891f2d21525f0a4ed19f5fb7
GokulakannanRN <Gokulakannan1306@gmail.com>
2026-08-31 17:53:59 +0530

    Destroy all

--- environments/production/main.tf
+++ environments/production/main.tf
@@ -1,3 +1,4 @@
+/*
 module "vpc" {
   source               = "../../modules/vpc"
   vpc_cidr             = "10.0.0.0/16"
@@ -19,3 +20,4 @@ module "s3" {
   env           = "production"
   s3_count      = 1
 }
+*/
