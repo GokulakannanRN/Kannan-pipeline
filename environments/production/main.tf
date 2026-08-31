ec3d3d756e9ae098aa3b38d1c8971a03aab3fe4e
GokulakannanRN <Gokulakannan1306@gmail.com>
2026-08-31 17:50:02 +0530

    modified

--- environments/production/main.tf
+++ environments/production/main.tf
@@ -1,4 +1,4 @@
-/*module "vpc" {
+module "vpc" {
   source               = "../../modules/vpc"
   vpc_cidr             = "10.0.0.0/16"
   subnet_cidr          = "10.0.1.0/24"
@@ -10,12 +10,12 @@ module "ec2" {
   ami_id        = "ami-0332d564d76dbd8d6"
   instance_type = "t3.micro"
   subnet_id     =  module.vpc.subnet_id
-  ec2_count     = 2
+  ec2_count     = 1
   env           = "production"
 }
 module "s3" {
   source        = "../../modules/s3"
   bucket        = "kannan-project"
   env           = "production"
-  s3_count      = 2
-}*/
+  s3_count      = 1
+}
